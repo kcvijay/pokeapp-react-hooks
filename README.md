007323275da66c892a439c50743537e5b7d04c70
Vijay KC <vj.kc2007@gmail.com>
2022-11-29 14:52:58 +0200

    Project almost finalized

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Pokemon Application made with React Hooks
 
-## This is a improved version of previous<a href="https://github.com/kcvijay/pokeapp_react" target="_blank"> Pokemon App</a>.
+## This is an improved version of previous<a href="https://github.com/kcvijay/pokeapp_react" target="_blank"> Pokemon App</a>.
 
 This version includes different React Hooks as opposed to previous version which included Class Components.
--- src/Components/PokeSingle.jsx
+++ src/Components/PokeSingle.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+// FIX:        Fix the problem while mapping the data for moves and types
+
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import classes from "./PokeSingle.module.css";
 
 const PokeSingle = () => {
-  return <div>Individual pokemons will be here</div>;
+  const params = useParams();
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${params.pokesingle}`)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className={classes.pokeSingleWrapper}>
+      <h3 className={classes.pokesingle_header}>{data.name}</h3>
+      <div className={classes.pokesingle_details}>
+        <img src={data.sprites?.other.home.front_default} alt={data.name}></img>
+        <table className={classes.pokesingle_table}>
+          <thead>
+            <tr>
+              <th>Features</th>
+              <th>Data</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Height</td>
+              <td>{data.height / 10} Meter</td>
+            </tr>
+
+            <tr>
+              <td>Weight</td>
+              <td>{data.weight / 10} Kg</td>
+            </tr>
+
+            <tr>
+              <td>Types</td>
+              <td>
+                {data.types
+                  .map((type) => {
+                    return type.type.name;
+                  })
+                  .join(", ")}
+              </td>
+            </tr>
+
+            <tr>
+              <td>Moves</td>
+              <td>{data.moves.length}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={classes.goBackWrapper}>
+        <button className="go-back" onClick={() => navigate(-1)}>
+          Go back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PokeSingle;
